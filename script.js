ea8d1c966822b94e640e581eca46d5cd1bde0866
Sagar <Sagar9980>
2021-02-09 17:49:21 +0545

    changed js

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slides");
-  let dots = document.getElementsByClassName("dots");
+  let dots = document.getElementsByClassName("dot");
+  console.log(dots.length);
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -23,8 +24,8 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace("active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += "active";
+  dots[slideIndex - 1].className += " active";
 }
